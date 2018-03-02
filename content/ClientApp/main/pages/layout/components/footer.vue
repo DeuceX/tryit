87ac74f8d67384ee28f850ece84e364a6e3f5270
Valery Kruglova <valery.vvk@gmail.com>
2018-03-02 18:32:41 +0200

    add footer, edit grid

---
+++ content/ClientApp/main/pages/layout/components/footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrap align flex-row">
+      <router-link :to="'/'" class="footer__link">About Us</router-link>
+      <router-link :to="'/'" class="footer__link">About Us</router-link>
+    </div>
+  </footer>
+</template>
